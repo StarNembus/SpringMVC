b684dca19fec9ed879ba1df53fea9cf036622a5d
StarNembus <larolimp@gmail.com>
2023-07-09 16:12:50 +0300

    Added Address

--- src/main/java/ru/larisa/spring/dao/PersonDAO.java
+++ src/main/java/ru/larisa/spring/dao/PersonDAO.java
@@ -38,12 +38,12 @@ public class PersonDAO {
 
     }
     public void save(Person person){
-        jdbcTemplate.update("INSERT INTO public.person(name, age, email) VALUES(?, ?, ?)", person.getName(),
-                person.getAge(), person.getEmail());
+        jdbcTemplate.update("INSERT INTO public.person(name, age, email, address) VALUES(?, ?, ?, ?)", person.getName(),
+                person.getAge(), person.getEmail(), person.getAddress());
     }
     public void update(int id, Person updatePerson){
-        jdbcTemplate.update("UPDATE public.person SET  name=?, age=?, email=? WHERE id=?", updatePerson.getName(),
-                updatePerson.getAge(), updatePerson.getEmail(), id);
+        jdbcTemplate.update("UPDATE public.person SET  name=?, age=?, email=?, address=? WHERE id=?", updatePerson.getName(),
+                updatePerson.getAge(), updatePerson.getEmail(), updatePerson.getAddress(), id);
     }
     public void delete(int id) throws SQLException {
         jdbcTemplate.update("DELETE FROM public.person WHERE id=?", id);
@@ -90,7 +90,7 @@ public class PersonDAO {
     private List<Person> create1000People() {
         List<Person> people = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
-            people.add(new Person(i, "Name" + i, 30, "test" + i + "mail.ru"));
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "mail.ru", "some address"));
         }
         return people;
     }
